5dd5f3f179d534bc9fb172a3451506d16c01a579
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-06-06 20:54:48 +0530

    done

--- WorkingProjects/DiabetesApp/app/train_model.py
+++ WorkingProjects/DiabetesApp/app/train_model.py
@@ -5,7 +5,7 @@ import os
 os.makedirs("models", exist_ok=True)  # Ensure the models directory exists
 # data = pd.read_csv("data\\diabetes.csv")
 # data = pd.read_csv("data/diabetes.csv")  # Relative path
-data = pd.read_csv(r"data\diabetes.csv")  # Use forward slashes for Linux-based systems (Docker)
+data = pd.read_csv(r"C:\Users\DELL\Documents\GitHub\Complete-Machine-Learning\WorkingProjects\DiabetesApp\app\data\diabetes.csv")  # Use forward slashes for Linux-based systems (Docker)
 
 X = data.drop('Outcome', axis =1)
 y = data['Outcome']
